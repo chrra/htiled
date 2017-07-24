53b4d12edaa7bda8b52880d243d991ddf79e5e1c
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2017-07-24 22:39:27 +0200

    Add nix shell file

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, base64-bytestring, bytestring, filepath
+      , hspec, hxt, QuickCheck, split, stdenv, vector, zlib
+      }:
+      mkDerivation {
+        pname = "htiled";
+        version = "0.1.4.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          base base64-bytestring bytestring filepath hxt split vector zlib
+        ];
+        testHaskellDepends = [ base hspec QuickCheck ];
+        description = "Import from the Tiled map editor";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
